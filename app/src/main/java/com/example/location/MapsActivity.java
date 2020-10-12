b3d0e1727a4655227703f3119d6f73f06476bb35
Hilman Septian <hilmansept@gmail.com>
2020-10-12 21:09:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/location/MapsActivity.java
@@ -0,0 +1,87 @@
+package com.example.location;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+
+
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Objects;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        // TODO: Consider calling
+        //    ActivityCompat#requestPermissions
+        // here to request the missing permissions, and then overriding
+        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+        //                                          int[] grantResults)
+        // to handle the case where the user grants the permission. See the documentation
+        // for ActivityCompat#requestPermissions for more details.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(Objects.requireNonNull(locationManager.getBestProvider
+                (criteria, false)));
+        if(location != null) {
+            Toast.makeText(this, "Lokasi Di Temukan", Toast.LENGTH_SHORT).show();
+            LatLng lokasi = new LatLng(location.getLatitude(),location.getLongitude());
+            googleMap.addMarker(new MarkerOptions().position(lokasi).title("Lattitude : " + location.getLatitude())
+                    .snippet("Longtitude: " + location.getLongitude()));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lokasi,13));
+        }else {
+            Toast.makeText(this, "Lokasi tidak di temukan", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void koordinat(View view) {
+
+    }
+
+    public void posisi(View view) {
+    }
+}
